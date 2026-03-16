c3d99b0118b2391e1d639d8922669cab632c7d80
nomanqadir-cosmoinc <nomanqadir@cosmoinc.co>
2026-03-16 13:26:52 +0500

    resolve the issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/home.jsx";
 import Layout from "./components/layouts";
 
 function App() {
